675cdb7c0bbf3a85a1a617d2884f154ac3ae298d
artsvet <61990390+artsvet@users.noreply.github.com>
2020-05-22 14:57:16 -0400

    Formatted for readability, pep8

--- avocado++.py
+++ avocado++.py
@@ -1,41 +1,68 @@
-import pandas as pd
 import os
+import pandas as pd
 
+
+'''Import and format
+
+Format Date as datetime values, group by region and type,
+return dict with group keys and AveragePrice series sorted by date. 
 '''
-import csv, format Date as datetime values, 
-group by region and type, return dict with 
-group keys and AveragePrice series sorted by date 
-'''
-avocadoTable = os.getcwd() + "\\avocado.csv"
-df = pd.read_csv(avocadoTable)\
+avocado_table = os.getcwd() + "\\avocado.csv"
+df = pd.read_csv(avocado_table)
 dated = df.assign(Date=pd.to_datetime(df['Date']))
-grouped = {y: x.sort_values('Date')['AveragePrice'] for y, x in df.groupby(['region', 'type'])}
+grouped = {
+    group: table.sort_values('Date')['AveragePrice']
+    for group, table in df.groupby(['region', 'type'])
+}
 
-'''
-for each series, calculate 10-week volatility, 
-10-week coefficient of variation 
-'''
-vol10 = {y: x.rolling(10).std(ddof=0).rename('vol10') for y, x in grouped.items()}
-cv10 = {y: x.div(grouped[y].mean()).rename('cv10') for y, x in vol10.items()}
+'''New variables
 
+For each series, calculate 10-week volatility, coefficient of variation. 
 '''
-calculate mean vol10 and cv10 for TotalUS by type
-'''
-vol10_US = {y[1]: x.mean() for y, x in vol10.items() if y[0]=='TotalUS'}
-cv10_US = {y[1]: x.mean() for y, x in cv10.items() if y[0]=='TotalUS'}
+vol10 = {
+    group: series.rolling(10).std(ddof=0).rename('vol10')
+    for group, series in grouped.items()
+}
+cv10 = {
+    group: series.div(grouped[group].mean()).rename('cv10')
+    for group, series in vol10.items()
+}
 
-'''
-for each group series, calculate 
-vol10, cv10 centered to TotalUS
-'''
-vol10_total = {y: x.sub(vol10_US[y[1]]).rename('vol10_total') for y, x in vol10.items()}
-cv10_total = {y: x.sub(cv10_US[y[1]]).rename('cv10_total') for y, x in cv10.items()}
+'''TotalUS reference mean
 
+Calculate mean vol10 and cv10 for TotalUS by type.
 '''
-collect ('region, 'type') series data
-write original table with new data to csv
+vol10_US = {
+    group_keys[1]: series.mean()
+    for group_keys, series in vol10.items()
+    if group_keys[0]=='TotalUS'
+}
+cv10_US = {
+    group_keys[1]: series.mean()
+    for group_keys, series in cv10.items()
+    if group_keys[0] == 'TotalUS'
+}
+
+'''Center new variables.
+
+For each group series, calculate vol10, cv10 centered to TotalUS.
 '''
-new = [pd.concat([y for x, y in s.items()]).sort_index() for s in [vol10, cv10, vol10_total, cv10_total]]
+vol10_centred = {
+    group: series.sub(vol10_US[group[1]]).rename('vol10_centred')
+    for group, series in vol10.items()
+}
+cv10_centred = {
+    group: series.sub(cv10_US[group[1]]).rename('cv10_centred')
+    for group, series in cv10.items()
+}
+
+'''Collect and export.
+
+Collect series data, join with original table and export.
+'''
+new = [
+    pd.concat([series for group, series in new_columns.items()])
+    for new_columns in [vol10, cv10, vol10_centred, cv10_centred]
+]
 final = pd.concat([df, *new], axis=1)
 final.to_csv(os.getcwd() + '\\avocado++.csv')
-
