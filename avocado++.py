db907e5d69f0c2a771edc1bf955794756981a7be
artsvet <61990390+artsvet@users.noreply.github.com>
2020-05-27 20:48:05 -0400

    more readable variable names

--- avocado++.py
+++ avocado++.py
@@ -10,9 +10,9 @@ return dict with group keys and AveragePrice series sorted by date.
 avocado_table = os.getcwd() + "\\avocado.csv"
 df = pd.read_csv(avocado_table)
 dated = df.assign(Date=pd.to_datetime(df['Date']))
-grouped = {
-    group: table.sort_values('Date')['AveragePrice']
-    for group, table in df.groupby(['region', 'type'])
+grouped_price = {
+    key: table.sort_values('Date')['AveragePrice']
+    for key, table in df.groupby(['region', 'type'])
 }
 
 '''New variables
@@ -20,27 +20,27 @@ grouped = {
 For each series, calculate 10-week volatility, coefficient of variation. 
 '''
 vol10 = {
-    group: series.rolling(10).std(ddof=0).rename('vol10')
-    for group, series in grouped.items()
+    key: series.rolling(10).std(ddof=0).rename('vol10')
+    for key, series in grouped_price.items()
 }
 cv10 = {
-    group: series.div(grouped[group].mean()).rename('cv10')
-    for group, series in vol10.items()
+    key: series.div(grouped_price[key].mean()).rename('cv10')
+    for key, series in vol10.items()
 }
 
 '''TotalUS reference mean
 
 Calculate mean vol10 and cv10 for TotalUS by type.
 '''
-vol10_US = {
-    group_keys[1]: series.mean()
-    for group_keys, series in vol10.items()
-    if group_keys[0]=='TotalUS'
+vol10_us = {
+    key[1]: us_price.mean()
+    for key, us_price in vol10.items()
+    if key[0]=='TotalUS'
 }
 cv10_US = {
-    group_keys[1]: series.mean()
-    for group_keys, series in cv10.items()
-    if group_keys[0] == 'TotalUS'
+    key[1]: us_price.mean()
+    for key, us_price in cv10.items()
+    if key[0] == 'TotalUS'
 }
 
 '''Center new variables.
@@ -48,12 +48,12 @@ cv10_US = {
 For each group series, calculate vol10, cv10 centered to TotalUS.
 '''
 vol10_centred = {
-    group: series.sub(vol10_US[group[1]]).rename('vol10_centred')
-    for group, series in vol10.items()
+    key: series.sub(vol10_us[key[1]]).rename('vol10_centred')
+    for key, series in vol10.items()
 }
 cv10_centred = {
-    group: series.sub(cv10_US[group[1]]).rename('cv10_centred')
-    for group, series in cv10.items()
+    key: series.sub(cv10_us[key[1]]).rename('cv10_centred')
+    for key, series in cv10.items()
 }
 
 '''Collect and export.
@@ -61,7 +61,7 @@ cv10_centred = {
 Collect series data, join with original table and export.
 '''
 new = [
-    pd.concat([series for group, series in new_columns.items()])
+    pd.concat([series for key, series in new_columns.items()])
     for new_columns in [vol10, cv10, vol10_centred, cv10_centred]
 ]
 final = pd.concat([df, *new], axis=1)
